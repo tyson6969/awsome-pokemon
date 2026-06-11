8cf3b6ad702855de71bdc79143dbf85b6fe928d8
ebrahim <abrhymjwyd19@gmail.com>
2026-06-11 13:27:15 +0400

    AYY TMX MAPS

--- code/main.py
+++ code/main.py
@@ -1,10 +1,23 @@
 from settings import *
+from pytmx.util_pygame import load_pygame 
+from os.path import join
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("awsome pokimoon")
+
+        self.import_assets()
+        self.setup(self.tmx_maps['world'], 'hosue')
+
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join( 'data', 'maps', 'world.tmx')) }
+        
+
+    def setup(self, tmx_map, player_start_pos):
+        for x,y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            print(x * TILE_SIZE, y * TILE_SIZE, surf)
 
     def run (self):
         while True:
