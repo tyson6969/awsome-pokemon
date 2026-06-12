89a38444d7802fa67696808fe9578e94022aef3e
ebrahim <abrhymjwyd19@gmail.com>
2026-06-12 08:19:10 +0400

    camera works ayy

--- code/main.py
+++ code/main.py
@@ -4,6 +4,7 @@ from os.path import join
 
 from sprites import Sprite
 from entites import Player
+from groups import ALLsprites
 
 class Game:
     def __init__(self):
@@ -13,7 +14,7 @@ class Game:
         self.clock = pygame.time.Clock()
 
         # shi group
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = ALLsprites()
 
         self.import_assets()
         self.setup(self.tmx_maps['world'], 'house')
@@ -28,7 +29,7 @@ class Game:
 
         for obj in tmx_map.get_layer_by_name("Entities"):
             if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
-                Player((obj.x, obj.y), self.all_sprites) 
+                self.player = Player((obj.x, obj.y), self.all_sprites) 
 
         
 
@@ -45,7 +46,7 @@ class Game:
 
             # SHI GAME LOGIC
             self.all_sprites.update(dt)
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
 
 
             pygame.display.update() 
