1949feb88094bb5bd14606d85e737b62d5fa0838
ebrahim <abrhymjwyd19@gmail.com>
2026-06-12 09:33:43 +0400

    animated water :3

--- code/sprites.py
+++ code/sprites.py
@@ -7,4 +7,12 @@ class Sprite(pygame.sprite.Sprite):
 
 class AnimatedSprite(Sprite):
     def __init__(self, pos, frames, groups):
-        super().__init__(pos, frames[0], groups)
+        self.frame_index, self.frames = 0, frames 
+        super().__init__(pos, frames[self.frame_index], groups)
+
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        self.image = self.frames[int(self.frame_index % len(self.frames))]
+
+    def update(self, dt):
+        self.animate(dt)
